8d871823cbfbac71935c3308e4db4ab12b52913d
Hugo BLASCO <hugoblasco@MacBook-Pro-de-Hugo.local>
2018-05-11 15:14:00 +0200

    mise en place des élements principaux textview et boutons

--- app/src/main/java/com/example/hugoblasco/blascobourja/MainActivity.java
+++ app/src/main/java/com/example/hugoblasco/blascobourja/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.hugoblasco.blascobourja;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_hw = (TextView)findViewById(R.id.tv_hello_world);
+        Button btn_hw = (Button)findViewById(R.id.btn_hello_world);
+        String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
+
+        tv_hw.setText(now);
+        btn_hw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),getString(R.string.msg),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        
     }
 }
